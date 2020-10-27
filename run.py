85c62a22f194a2bf7416cd2c3d508ea926c04a1f
Gildemberg Santos <gildemberg.santos@gmail.com>
2020-10-27 11:03:15 -0300

    Arquivos run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # run.py
 #
 # Copyright 2020 Gildemberg Santos <gildemberg.santos@gmail.com>
+# Copyright 2020 Alexandre Correia <alexandrecorreia234@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
