42f32281a6db691da33e1bb302a1330855085b19
Gildemberg Santos <gildemberg.santos@gmail.com>
2020-10-30 11:40:06 -0300

    ajustes

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
-#! /usr/bin/bash
+#! /bin/bash
+## Cria o venv
+# clear
+# sudo dnf install virtualenv
+# virtualenv venv -p /usr/bin/python3.9
 clear
 EXISTEM[0]=$(python --version)
 source venv/bin/activate
+venv/bin/pip3 freeze > requirements.txt
 EXISTEM[1]=$(python --version)
 echo "Global" ${EXISTEM[0]} " | Local" ${EXISTEM[1]}
 echo "--> Programa executado"
